9efc809cde27baa316ac461bac101dfc3a6b0f5c
Ankit Bansal <81725794+theankitbansal@users.noreply.github.com>
2023-01-01 08:54:42 +0530

    Time: 4 ms (32.33%), Space: 6.5 MB (53.35%) - LeetHub

---
+++ 0290-word-pattern/0290-word-pattern.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool wordPattern(string pattern, string s) {
+        map<char, int>p;
+        
+        map<string, int>w;
+        istringstream in(s);
+        int i=0, n=pattern.size();
+        
+        for(string word; in>>word; i++){
+            if(i==n || p[pattern[i]]!=w[word])
+                return false;
+            p[pattern[i]]=w[word]=i+1;
+        }
+        return i==n;
+        
+    }
+};
