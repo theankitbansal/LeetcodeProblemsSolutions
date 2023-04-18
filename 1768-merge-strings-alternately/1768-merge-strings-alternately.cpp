6532c23ac489371efb39d3367aafbca7d19a5325
Ankit Bansal <81725794+theankitbansal@users.noreply.github.com>
2023-04-18 09:26:17 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (60.87%) - LeetHub

---
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        int i=0, j=0;
+        string s="";
+        while(i<word1.length() && j<word2.length()){
+            s+=word1[i++];
+            s+=word2[j++];
+        }
+        
+        while(i<word1.length())
+            s.push_back(word1[i++]);
+        
+        while(j<word2.length())
+            s.push_back(word2[j++]);
+        
+        return s;
+    }
+};
